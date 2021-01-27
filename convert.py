9d3ca1c73e0132427bdb4b586bc32e3863a35195
Haneul-Yoo <skysky2387@gmail.com>
2021-01-27 17:28:08 +0900

    Convert 100

--- convert.py
+++ convert.py
@@ -34,7 +34,7 @@ def get_kialo(data):
     cnt = 0
     for row in data:
         cnt += 1
-        if cnt >= 100:
+        if cnt > 100:
             break
         book = {
             'id': row['claim_id'],
